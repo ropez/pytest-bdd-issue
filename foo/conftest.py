541c0fbe5bd3940d129d902ace27e96250dab94b
Anatoly Bubenkov <bubenkoff@gmail.com>
2014-09-11 07:55:13 +0000

    more hooks

--- foo/conftest.py
+++ foo/conftest.py
@@ -1,4 +1,7 @@
 print "\n", __name__
 
 def pytest_pyfunc_call(pyfuncitem):
-    print 'hook!'
+    print 'pytest_pyfunc_call hook'
+
+def pytest_generate_tests(metafunc):
+    print 'pytest_generate_tests hook'
